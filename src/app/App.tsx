13b9efb4550c2700d5d12153affce416583182ec
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-08-29 12:29:50 -0700

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header"
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen m-0 flex flex-col items-center justify-start">
+      <Header />
+      App Here
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <div className="w-full h-14 bg-slate-300">
+            Header
+        </div>
+    )
+}
+
+export default Header;
